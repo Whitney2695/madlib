e81b68347cd44a6aff3fbb2a1c44deca708969b9
Whitney Navalia <136854331+Whitney2695@users.noreply.github.com>
2023-11-22 15:57:10 -0800

    madlib game

---
+++ madlib.py
@@ -0,0 +1,6 @@
+color = input("enter a color:")
+plural_noun = input("Enter a plural noun:")
+celebrity = input("enter a celebrity:")
+print("roses are" +" " +color)
+print(plural_noun+ " " + "are blue")
+print("i love" +" "+ celebrity)
